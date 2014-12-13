787c71b00f9c106da7dda5959c275952a8b60f8a
ramimoshe <rms.rami.m@gmail.com>
2014-12-13 22:50:59 +0200

    add gitignore

--- ReverseTicTacToe/TicTacToeConsoleUI.cs
+++ ReverseTicTacToe/TicTacToeConsoleUI.cs
@@ -7,27 +7,60 @@ namespace ReverseTicTacToe
 {
     static class TicTacToeConsoleUI
     {
-        private static TicTacToe Game;
+        private static TicTacToe m_game;
         
         public static void Start()
         {
+            int size = getBoardSize();
+            m_game = new TicTacToe(size);
+            playerType playerType = getOpponentType();
 
+            while(true)
+            {
+                
+            }
 
         }
 
-        private static void getBoardSize()
+        private static int getBoardSize()
         {
+            Console.WriteLine("Enter board size (3-9)");
+            char input = Console.Read();
+            while (!Char.IsNumber(input) || Char.GetNumericValue(input) < 3)
+            {
+                Console.WriteLine("Invalid input, Enter board size (3-9)");
+                char input = Console.Read();
+            }
 
+            return Char.GetNumericValue(input);
         }
 
         private static playerType getOpponentType()
         {
-            return playerType.Computer;
+            Console.WriteLine("Enter player type (1 = human, 2 = PC)");
+            char input = Console.Read();
+            while (!Char.IsNumber(input) || Char.GetNumericValue(input) < 1 || Char.GetNumericValue(input) > 2)
+            {
+                Console.WriteLine("Invalid input, Enter player type (1 = human, 2 = PC)"");
+                char input = Console.Read();
+            }
+
+            return (playerType)Char.GetNumericValue(input);
         }
 
         private static void printBoard()
         {
+            int rowLength = m_game..GetLength(0);
+            int colLength = arr.GetLength(1);
 
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    Console.Write(string.Format("{0} ", arr[i, j]));
+                }
+                Console.Write(Environment.NewLine + Environment.NewLine);
+            }
         }
 
         private static void printScores()
