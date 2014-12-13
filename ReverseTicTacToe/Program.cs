6eeede7d5dc0948e8ed5f7513df45f31d8c8d06b
boris <boris@firstofferz.com>
2014-12-13 17:32:04 +0200

    first commit

---
+++ ReverseTicTacToe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Ex02.ConsoleUtils;
+
+namespace ReverseTicTacToe
+{
+    class Program
+    {
+        public void Main()
+        {
+
+        }
+    
+    
+    }
+}
